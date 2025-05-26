4f61afbf2e909a3185d453151e1fc8910c34e03a
Michal Lurie <Michal.Lurie@gmail.com>
2025-05-26 16:47:21 +0200

    Refac: Nicer structure

--- include/incerr.hpp
+++ include/incerr.hpp
@@ -1,24 +1,34 @@
 #pragma once
 
+#include <source_location>
 #include <string_view>
 #include <system_error>
 #include <utility>
 
+
 namespace incom {
 namespace error {
 namespace detail {
-template <typename T>
-class err_category_impl : public std::error_category {
-private:
-    err_category_impl() = default;
 
-    virtual const char *name() const noexcept override { return "plotSpecs Error"; }
+template <typename T>
+class incerr_cat : public std::error_category {
+private:
+    incerr_cat() = default;
+
+    template <typename TT>
+    constexpr auto __typeToString() {
+        auto EmbeddingSignature = std::string{std::source_location::current().function_name()};
+        auto firstPos           = EmbeddingSignature.rfind("::") + 2;
+        return EmbeddingSignature.substr(firstPos, EmbeddingSignature.size() - firstPos - 1);
+    }
+
+    virtual const char *name() const noexcept override { return __typeToString<T>(); }
     virtual std::string message(int ev) const override { return (incerr_msg_dispatch(T{ev})); }
 
 public:
     // Meyers' Singleton technique to guarantee only 1 instance is ever created
     static const std::error_category &getSingleton() {
-        static const err_category_impl<T> instance;
+        static const incerr_cat<T> instance;
         return instance;
     }
 };
@@ -36,16 +46,20 @@ inline const std::string incerr_msg_dispatch(E &&e) {
 } // namespace detail
 
 class incerr_code : public std::error_code {
-    template <typename E>
-    requires std::is_scoped_enum_v<E>
-    friend inline const incerr_code make_incerr_code(E e);
-
-    template <typename E>
-    requires std::is_scoped_enum_v<E>
-    friend inline const incerr_code make_incerr_code(E e, std::string_view sv);
-
-private:
+public:
     const std::string localMsg;
+
+    template <typename E>
+    requires std::is_scoped_enum_v<E>
+    inline const incerr_code make_incerr_code(E e) {
+        return incerr_code(std::to_underlying(e), error::detail::incerr_cat<E>::getSingleton());
+    }
+
+    template <typename E>
+    requires std::is_scoped_enum_v<E>
+    inline const incerr_code make_incerr_code(E e, std::string_view sv) {
+        return incerr_code(std::to_underlying(e), error::detail::incerr_cat<E>::getSingleton(), sv);
+    }
 
 private:
     incerr_code(int ec, const std::error_category &cat) noexcept : std::error_code(ec, cat), localMsg() {}
@@ -53,29 +67,17 @@ private:
         : std::error_code(ec, cat), localMsg(localMsg) {}
 };
 
-template <typename E>
-requires std::is_scoped_enum_v<E>
-inline const std::error_code make_error_code(E e) {
-    return std::error_code(std::to_underlying(e), error::detail::err_category_impl<E>::getSingleton());
-}
-template <typename E>
-requires std::is_scoped_enum_v<E>
-inline const std::error_code make_error_code(E e, std::string_view sv) {
-    return std::error_code(std::to_underlying(e), error::detail::err_category_impl<E>::getSingleton());
-}
+// template <typename E>
+// requires std::is_scoped_enum_v<E>
+// inline const std::error_code make_error_code(E e) {
+//     return std::error_code(std::to_underlying(e), error::detail::incerr_cat<E>::getSingleton());
+// }
+// template <typename E>
+// requires std::is_scoped_enum_v<E>
+// inline const std::error_code make_error_code(E e, std::string_view sv) {
+//     return std::error_code(std::to_underlying(e), error::detail::incerr_cat<E>::getSingleton());
+// }
 
-
-template <typename E>
-requires std::is_scoped_enum_v<E>
-inline const incerr_code make_incerr_code(E e) {
-    return incerr_code(std::to_underlying(e), error::detail::err_category_impl<E>::getSingleton());
-}
-
-template <typename E>
-requires std::is_scoped_enum_v<E>
-inline const incerr_code make_incerr_code(E e, std::string_view sv) {
-    return incerr_code(std::to_underlying(e), error::detail::err_category_impl<E>::getSingleton(), sv);
-}
 
 } // namespace error
 } // namespace incom
